1b27f2b7578b525f5010177180b0d7b46d1e1bf4
zolars <zolars@outlook.com>
2020-02-02 17:12:51 +0800

    2020-02-02 17:12 modify account

--- app/page.py
+++ app/page.py
@@ -1,4 +1,4 @@
-import inspect
+import json
 import logging
 
 from flask import (Blueprint, flash, g, redirect, render_template, request,
@@ -6,38 +6,10 @@ from flask import (Blueprint, flash, g, redirect, render_template, request,
 from werkzeug.exceptions import abort
 
 # from app.auth import login_required
+from app.utils import *
 from app.db import get_db, close_db
 
 bp = Blueprint('page', __name__)
-
-
-# Record page_history
-def record_page_history(page_path, user_ip):
-    import json
-
-    db = get_db()
-    if g.user == '{}' or g.user is None:
-        user_id = None
-    else:
-        user_id = json.loads(g.user)['id']
-
-    if user_id is not None:
-        db.execute(
-            'INSERT INTO page_history (user_id, user_ip, page_path, time) VALUES ("{user_id}", "{user_ip}", "{page_path}", {time})'
-            .format(user_id=user_id,
-                    user_ip=user_ip,
-                    page_path=page_path,
-                    time="now()"))
-        db.commit()
-    else:
-        db.execute(
-            'INSERT INTO page_history (user_id, user_ip, page_path, time) VALUES (null, "{user_ip}", "{page_path}", {time})'
-            .format(user_ip=user_ip, page_path=page_path, time="now()"))
-        db.commit()
-    flash('记录页面信息("{user_id}", {user_ip}, "{page_path}", {time})'.format(
-        user_id=user_id, user_ip=user_ip, page_path=page_path, time="now()"))
-
-    close_db()
 
 
 @bp.route('/', methods=('GET', 'POST'))
